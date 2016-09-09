2dc7ac22013d48c776c6a1251a62310c5f3092b2
Gija <gkvantsjiani@roc-dev.com>
2016-09-09 11:01:50 +0200

    message

---
+++ src/Hoofdstuk2/Opdracht1.java
@@ -0,0 +1,15 @@
+package Hoofdstuk2;
+
+import java.applet.Applet;
+        import java.awt.*;
+
+public class Opdracht1 extends Applet {
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Gia", 20, 20);
+    }
+}
